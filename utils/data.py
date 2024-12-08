35ad0c437a3fcdbb2380eb349262ae3828b16cf0
sifal <sifalklioui@yahoo.com>
2024-12-08 18:33:26 +0100

    clean

--- utils/data.py
+++ utils/data.py
@@ -6,10 +6,12 @@ import torch
 import matplotlib.pyplot as plt
 import pickle
 
+
 class TransformSubset(Dataset):
     """
     Wrapper for applying transformations to a Subset.
     """
+
     def __init__(self, subset, transform):
         self.subset = subset
         self.transform = transform
@@ -22,6 +24,7 @@ class TransformSubset(Dataset):
 
     def __len__(self):
         return len(self.subset)
+
 
 class PokemonDataModule(Dataset):
     def __init__(self, data_dir):
@@ -52,7 +55,7 @@ class PokemonDataModule(Dataset):
         # Iterate over the dataloader to get a batch of data
         for data, labels in dataloader:
             # Take the first n_rows * n_cols samples from the batch
-            for i, ax in enumerate(axes[:n_rows * n_cols]):
+            for i, ax in enumerate(axes[: n_rows * n_cols]):
                 if i >= len(data):  # If fewer samples than the grid size, stop
                     break
 
@@ -67,7 +70,7 @@ class PokemonDataModule(Dataset):
 
                 ax.imshow(img)
                 ax.set_title(self.class_names[label.item()])
-                ax.axis('off')
+                ax.axis("off")
             break  # Only process the first batch
 
         plt.tight_layout()
@@ -85,7 +88,7 @@ class PokemonDataModule(Dataset):
         Returns:
             Tensor: Denormalized image tensor.
         """
-        return img * stats['std'].view(-1, 1, 1) + stats['mean'].view(-1, 1, 1)
+        return img * stats["std"].view(-1, 1, 1) + stats["mean"].view(-1, 1, 1)
 
     def _get_stats(self, dataset):
         """
@@ -97,17 +100,14 @@ class PokemonDataModule(Dataset):
             for data, _ in dataloader:
                 data.cuda()
                 total_sum += data.sum(dim=(0, 2, 3))
-                total_squared_sum += (data ** 2).sum(dim=(0, 2, 3))
+                total_squared_sum += (data**2).sum(dim=(0, 2, 3))
                 total_count += data.size(0) * data.size(2) * data.size(3)
 
             means = total_sum / total_count
-            stds = torch.sqrt((total_squared_sum / total_count) - (means ** 2))
-        return {'mean':means, 'std':stds}
+            stds = torch.sqrt((total_squared_sum / total_count) - (means**2))
+        return {"mean": means, "std": stds}
 
-    def prepare_data(self,
-                     indices_file='indices.pkl',
-                     get_stats=False
-                    ):
+    def prepare_data(self, indices_file="indices.pkl", get_stats=False):
         """
         Prepare train and test dataloaders with optional transformations.
 
@@ -120,7 +120,7 @@ class PokemonDataModule(Dataset):
             tuple: trainloader, testloader
         """
         try:
-            with open(indices_file, 'rb') as f:
+            with open(indices_file, "rb") as f:
                 self.train_indices, self.test_indices = pickle.load(f)
         except (EOFError, FileNotFoundError):
             # Generate new indices if file is empty or doesn't exist
@@ -128,13 +128,13 @@ class PokemonDataModule(Dataset):
                 range(len(self.dataset)),
                 test_size=0.2,
                 stratify=self.dataset.targets,
-                random_state=42
+                random_state=42,
             )
 
             # Ensure directory exists before saving
-            os.makedirs(os.path.dirname(indices_file) or '.', exist_ok=True)
+            os.makedirs(os.path.dirname(indices_file) or ".", exist_ok=True)
 
-            with open(indices_file, 'wb') as f:
+            with open(indices_file, "wb") as f:
                 pickle.dump([self.train_indices, self.test_indices], f)
 
         # Prepare train and test subsets
@@ -143,33 +143,49 @@ class PokemonDataModule(Dataset):
 
         return self._get_stats(self.train_dataset) if get_stats else None
 
-    def get_dataloaders(self,
-                    train_transform=None,
-                    test_transform=None,
-                    train_batch_size=None,
-                    test_batch_size=None):
-            """
-            Prepare train and test dataloaders with optional transformations.
+    def get_dataloaders(
+        self,
+        train_transform=None,
+        test_transform=None,
+        train_batch_size=None,
+        test_batch_size=None,
+    ):
+        """
+        Prepare train and test dataloaders with optional transformations.
 
-            Args:
-                train_transform (callable): Transformation to apply to training data.
-                train_batch_size (int): Batch size for the training dataloader.
-                validation_batch_size (int): Batch size for the validation dataloader.
+        Args:
+            train_transform (callable): Transformation to apply to training data.
+            train_batch_size (int): Batch size for the training dataloader.
+            validation_batch_size (int): Batch size for the validation dataloader.
 
-            Returns:
-                tuple: trainloader, testloader
-            """
-            assert self.train_dataset is not None, "You need to call `prepare_data` before using `get_dataloaders`."
+        Returns:
+            tuple: trainloader, testloader
+        """
+        assert (
+            self.train_dataset is not None
+        ), "You need to call `prepare_data` before using `get_dataloaders`."
 
-            # Default batch sizes if not provided
-            test_batch_size = train_batch_size if test_batch_size is None else test_batch_size
+        # Default batch sizes if not provided
+        test_batch_size = (
+            train_batch_size if test_batch_size is None else test_batch_size
+        )
 
-            # Wrap subsets in a transformed dataset if transformations are provided
-            train_dataset = TransformSubset(self.train_dataset, train_transform) if train_transform else self.train_dataset
+        # Wrap subsets in a transformed dataset if transformations are provided
+        train_dataset = (
+            TransformSubset(self.train_dataset, train_transform)
+            if train_transform
+            else self.train_dataset
+        )
 
-            test_dataset = TransformSubset(self.test_dataset, test_transform) if test_transform else self.test_dataset
+        test_dataset = (
+            TransformSubset(self.test_dataset, test_transform)
+            if test_transform
+            else self.test_dataset
+        )
 
-            trainloader = DataLoader(train_dataset, batch_size=train_batch_size, shuffle=True)
-            testloader = DataLoader(test_dataset, batch_size=test_batch_size, shuffle=False)
+        trainloader = DataLoader(
+            train_dataset, batch_size=train_batch_size, shuffle=True
+        )
+        testloader = DataLoader(test_dataset, batch_size=test_batch_size, shuffle=False)
 
-            return trainloader, testloader
+        return trainloader, testloader
